17812a4d4633c4204b91dc161835d170ae05d1b9
Emilie - G <guthier.emilie@gmail.com>
2026-02-10 01:21:20 +0100

    premier push

---
+++ EfghDev/Models/PortfolioItem.cs
@@ -0,0 +1,12 @@
+﻿namespace EfghDev.Models
+{
+    public class PortfolioItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string Tags { get; set; } = string.Empty; // Ex: "C#, Blazor, SQL"
+        public string ImageUrl { get; set; } = string.Empty; // Optionnel si tu as des screenshots
+        public string Category { get; set; } = "Autre";
+    }
+}
